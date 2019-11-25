2e6cad194dbd182905d3356bf089474e780b5f08
wally <wallychung@aibrain.co.kr>
2019-11-25 16:19:36 +0900

    updated

--- create.js
+++ create.js
@@ -1,19 +1,4 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
-
-
-AWS.config.update({region: 'us-east-2'});
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
-  const data = JSON.parse(event.body);
-
-  const params = {
-    TableName: process.env.tableName,
-
-import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
